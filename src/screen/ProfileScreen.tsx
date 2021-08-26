f56745c98646f07b1d324e19b47f218fa16cca6e
froggydisk <sewoomkim@naver.com>
2021-08-26 16:44:14 +0900

    node.js

--- src/screen/ProfileScreen.tsx
+++ src/screen/ProfileScreen.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import {StyleSheet, Text, View} from "react-native";
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, Button, TextInput} from "react-native";
 
 function ProfileScreen() {
-    return (
-        <View style={styles.container}>
-          <Text style={styles.font}>Profile</Text>
-        </View>
-      );
-    }
-    
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        backgroundColor: '#F5FCFF',
-        alignItems: 'center',
-        justifyContent: 'center',
+
+  const [userEmail, setUserEmail] = useState('');
+
+  const onclick = () => {
+    const textbox = {
+      inText: userEmail,
+    };
+    fetch("http://localhost:5000/text", { 
+      method: "post", //통신방법
+      headers: {
+        Accept: 'application/json', 
+        "content-type": "application/json",
       },
-      font: {
-        fontSize: 22,
-        marginBottom: 10,
-      },
+      body: JSON.stringify(textbox), //textbox라는 객체를 보냄
     });
+  };
+
+  return (
+    <View>
+      <TextInput style={styles.input} onChangeText={(UserEmail) => setUserEmail(UserEmail)}/>
+      <Button onPress={onclick} title='전송'/>
+    </View>
+  );
+  
+}
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
+
 export default ProfileScreen;
---
+++ src/server/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/text", (req, res) => { //데이터 받는 곳
+  const text1 = req.body.inText;
+  console.log(text1);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
